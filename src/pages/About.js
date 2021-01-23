0f268b6569f42db085620cb5844907e0e223b9c0
Damilare Oyedeji <fuglydami@gmail.com>
2021-01-23 10:37:14 +0100

    adding new files

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur,
+        voluptas. Eligendi incidunt dignissimos vitae cumque reprehenderit
+        voluptas iste repellat fugiat, officiis deserunt libero numquam
+        quibusdam molestias quae ipsa atque minus inventore esse culpa eos illo
+        eveniet accusamus. Soluta assumenda quos molestiae? Perferendis vel
+        dolor itaque. Doloremque mollitia iusto quidem laudantium.
+      </p>
+    </section>
+  )
+}
+
+export default About
